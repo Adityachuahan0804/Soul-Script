6356572fbdd60bc553f0b14f0de3d08b741e6cae
ADITYA <adityachauhan0804@gmail.com>
2025-09-18 18:31:43 +0530

    Add files via upload

---
+++ SoulScript-main/SoulScript-main/src/main/java/com/anku/journalApp/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.anku.journalApp.repository;
+
+import com.anku.journalApp.entity.User;
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface UserRepository extends MongoRepository<User, ObjectId> {
+    User findByUserName(String userName);
+}
